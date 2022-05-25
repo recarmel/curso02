dd289efec62c9a495c1a6a194f0a470d1176c25c
recarmel <melvincardona@gmail.com>
2022-05-24 19:51:28 -0600

    segunda cambio

--- TestAutomation/src/test/java/pruebasunitarias/TextCaseConvert.java
+++
@@ -1,9 +0,0 @@
-package pruebasunitarias;
-public class TextCaseConvert {
-    public String convertirTextoMayusculas(String text){
-        return text.toUpperCase();
-    }
-    public String convertirTextoMinusculas(String text){
-        return text.toLowerCase();
-    }
-}
